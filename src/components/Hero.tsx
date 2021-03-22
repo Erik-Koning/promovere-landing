42bc71fa1252ac0ee7077b0afc7a3abe532d582f
Erik Koning <erik@koning.ca>
2021-03-22 15:49:17 -0400

    video ad

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, Heading, Text, VStack } from '@chakra-ui/react';
+import { Grid, GridItem, Button, Heading, Text, VStack, AspectRatio } from '@chakra-ui/react';
 import TextLoop from 'react-text-loop';
 import { Wrapper } from './Wrapper';
 import { Link } from 'react-scroll';
@@ -21,20 +21,35 @@ const TextAnimation: React.FC = () => {
 export const Hero: React.FC = () => {
 	return (
 		<Wrapper bg='brand' pt='10' pb='10'>
-			<VStack py={12} spacing={4} align='start' width="min-content">
-				<Heading as='h2' size='2xl' lineHeight='1.3' width="max-content">
-					Leading the new<br /> generation of agile<br /> <TextAnimation />
-				</Heading>
-				<Text>
-					Promovere is a digital marketplace for businesses that increases employee engagement, retention and
-					growth
-				</Text>
-				<Link to='booking' smooth={true}>
-					<Button bg='white' boxShadow="base" _hover={{ bg: "white", boxShadow: "lg"}}>
-						Learn More
-					</Button>
-				</Link>
-			</VStack>
+			
+				<Grid h="100%" templateColumns="repeat(2, 1fr)" gap={6}>
+					<GridItem colSpan={1}>
+					<VStack py={12} spacing={4} align='start' width="min-content">
+						<Heading as='h2' size='2xl' lineHeight='1.3' width="max-content">
+							Leading the new<br /> generation of agile<br /> <TextAnimation />
+						</Heading>
+						<Text>
+							Promovere is a digital marketplace for businesses that increases employee engagement, retention and
+							growth
+						</Text>
+						<Link to='booking' smooth={true}>
+							<Button bg='white' boxShadow="base" _hover={{ bg: "white", boxShadow: "lg"}}>
+								Learn More
+							</Button>
+						</Link>
+					</VStack>
+					</GridItem>
+					<GridItem colSpan={1}>
+						<AspectRatio maxW="100%" ratio={1.775}>
+						<iframe
+							title="naruto"
+							src="https://www.youtube.com/embed/AiJufeNL6ms"
+							allowFullScreen
+						/>
+						</AspectRatio>
+					</GridItem>
+				</Grid>
+			
 		</Wrapper>
 	);
 };
